d1bd30fe22d53f33d1869da8760c976de3f80a34
Humpnduati <nduatihump@gmail.com>
2025-05-31 04:51:36 +0300

    ..

--- practise/1.2 practise.py
+++ practise/1.2 practise.py
@@ -7,4 +7,19 @@ x = 2
 print(x)
 x = x + 2
 print(x)
-# this is an example of a flow chart
+
+
+# this is an example of a conditional steps
+x = 6
+if x < 5:
+    print('smaller')
+if x > 5:
+    print('bigger')
+
+#this is an example of a loop
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print ('blastoff!')
+
